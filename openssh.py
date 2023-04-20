79994d895a536836d31acee7975cc3dcabdf10df
CyberVPN <120331083+Azigaming404@users.noreply.github.com>
2023-04-20 15:18:43 +0700

    Update openssh.py

--- openssh.py
+++ openssh.py
@@ -5,7 +5,7 @@ LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 8880  
+  LISTENING_PORT = 80 
 #Pass
 PASS = ''
 
@@ -13,8 +13,8 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 #port opensssh
-DEFAULT_HOST = '127.0.0.1:69'
-RESPONSE = 'HTTP/1.1 101 <font color="green">CyberVPN</font>\r\nContent-Length: 104857600000\r\n\r\n'
+DEFAULT_HOST = '127.0.0.1:88'
+RESPONSE = 'HTTP/1.1 101 HTTP to OpenSSH Bye HideSSH.com\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 class Server(threading.Thread):
